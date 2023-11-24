9a3f10d79e88b6e02ff5cf661d77d0c7e6b76f76
okozhukh <143549248+okozhukh@users.noreply.github.com>
2023-11-24 21:03:45 +0000

    1,2

---
+++ HW9.js
@@ -0,0 +1,15 @@
+//Завдання 1
+$(document).ready(function() {
+    $('h2.head').css('background-color', 'green');
+  
+    $('h2.head .inner').css('font-size', '35px');
+  });
+  
+
+//Завдання 2
+$(document).ready(function() {
+    $('a[href^="https"]').attr('target', '_blank');
+  });
+  
+
+//Завдання 3
